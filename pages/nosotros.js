b63c956cdc68f8540fbbe2e7e417d80517a9f9e4
Mikail Castro <russbellc@gmail.com>
2022-04-27 13:30:24 -0500

    primer commit guitarLAB con NEXT.JS

---
+++ pages/nosotros.js
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+const Nosotros = () => {
+	return (
+		<Layout pagina="Nosotros">
+			<h1>Desde nosotros</h1>
+		</Layout>
+	);
+};
+
+export default Nosotros;
